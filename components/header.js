acf094fd3d74fc3bfb991cafff76662032c09952
Yuri Gor <yuri.horus@gmail.com>
2019-01-31 23:51:52 +0300

    favicon

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ export default () => (
   <header>
     <Head>
       <title>Deepdash - traversal extension for Lodash</title>
-      <link rel="icon" type="image/png" href="/static/favicon.png" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
     </Head>
     <h1>Static Next.js Example on Now 2.0</h1>
   </header>
--- next.config.js
+++ next.config.js
@@ -1,1 +1,16 @@
-module.exports = {}
+const fs = require('fs');
+const { join } = require('path');
+const { promisify } = require('util');
+
+const copyFile = promisify(fs.copyFile);
+
+module.exports = {
+  async exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    if (dev) {
+      return defaultPathMap;
+    }
+    // This will copy robots.txt from your project root into the out directory
+    await copyFile(join(dir, 'favicon.png'), join(outDir, 'favicon.png'));
+    return defaultPathMap;
+  },
+};
