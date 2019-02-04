79ed5122ed6b972bf0c8bdf9bb33cbffa644fa7f
Yuri Gor <YuriGor@users.noreply.github.com>
2019-02-04 14:00:25 +0300

    Highlightjs

--- components/header.js
+++ components/header.js
@@ -8,6 +8,8 @@ export default () => (
       <link rel="icon" type="image/png" href="/favicon.png" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/styles/default.min.css">
+      <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/highlight.min.js"></script>
     </Head>
     <div className="sand" />
     <h1>
